2973b9a0144b36ca692191c762f890e4b5680975
Sunayna Singh <me@sunayna.com>
2024-01-16 15:30:35 -0800

    retain volts for all states + manual periodic

--- src/main/cpp/Climb/Climb.cpp
+++ src/main/cpp/Climb/Climb.cpp
@@ -1,46 +1,53 @@
 #include "Climb/Climb.h"
 
-//maybe PID ff or some other fancier motor control
-
 void Climb::CorePeriodic(){
     UpdatePos();
-    double voltage = 0;
-    if (m_state == MOVING){
-        double targPos;
-        switch (m_targ){
-            case STOWED:
-                voltage = -MAX_VOLTS;
-                targPos = MIN_POS;
-                break;
-            case EXTENDED:
-                voltage = MAX_VOLTS;
-                targPos = MAX_POS;
-                break;
-            case CLIMB:
-                voltage = CLIMB_VOLTS;
-                targPos = MIN_POS;
-                break;
-        }
-        if (AtTarget(targPos))
-            m_state = AT_TARGET;
+}
+
+void Climb::CoreTeleopPeriodic(){
+    StateInfo info;
+    double targPos;
+    switch (m_targ){
+        case STOWED:
+            info = STOW_INFO;
+            targPos = MIN_POS;
+            break;
+        case EXTENDED:
+            info = EXTENDED_INFO;
+            targPos = MAX_POS;
+            break;
+        case CLIMB:
+            info = CLIMB_INFO;
+            targPos = MIN_POS;
+            break;
     }
-    else if (m_state == AT_TARGET && m_targ == CLIMB)
-        voltage = HOLD_VOLTS;
+
+    double voltage = 0.0;
+    if (m_state == AT_TARGET){
+        voltage = info.RETAIN_VOLTS;
+    } else if (m_state == MOVING){
+        voltage = info.MOVE_VOLTS;
+    }
+    voltage = std::clamp(voltage, -MAX_VOLTS, MAX_VOLTS);
+
+    if (AtTarget(targPos))
+        m_state = AT_TARGET;
+   
     m_master.SetVoltage(units::volt_t(voltage));
 }
 
 void Climb::UpdatePos(){
-    // m_pos = m_master.GetPosition().GetValueAsDouble();
     m_pos = m_absEncoder.GetAbsolutePosition();
 }
 
 bool Climb::AtTarget(double target){
     if (std::abs(m_pos-target) <= POS_TOLERANCE)
         return true;
-    else return false;
+    return false;
 }
 
 Climb::Climb(){
+    m_master.SetNeutralMode(ctre::phoenix6::signals::NeutralModeValue::Brake);
     // m_slave.SetControl(Follower(Ids::MASTER_CLIMB_MOTOR, false));
 }
 
@@ -66,13 +73,30 @@ void Climb::SetTarget(Target t){
     m_targ = t;
 }
 
+void Climb::ManualPeriodic(double voltage){
+    voltage = std::clamp(voltage, -MAX_VOLTS, MAX_VOLTS);
+    m_master.SetVoltage(units::volt_t(voltage));
+}
+
 void Climb::CoreShuffleboardPeriodic(){
-    m_shuff.PutNumber("Cur pos", m_pos);
-    m_shuff.PutNumber("targ", m_targ);
+    m_shuff.PutNumber("cur pos", m_pos);
+    m_shuff.PutNumber("targ state", m_targ);
     m_shuff.PutNumber("state", m_state);
-    m_shuff.add("Extended pos", &MAX_POS, true);
-    m_shuff.add("Stowed pos", &MIN_POS, true);
-    m_shuff.add("Extend stow volts", &MAX_VOLTS, true);
-    m_shuff.add("Climb volts", &CLIMB_VOLTS, true);
-    m_shuff.add("Hold volts", &HOLD_VOLTS, true);
+
+    m_shuff.add("max pos", &MAX_POS, true);
+    m_shuff.add("min pos", &MIN_POS, true);
+    m_shuff.add("max volts", &MAX_VOLTS, true);
+    m_shuff.add("tolerance", &POS_TOLERANCE, true);
+
+    m_shuff.add("climb pos", &CLIMB_INFO.TARG_POS, true);
+    m_shuff.add("climb volts", &CLIMB_INFO.MOVE_VOLTS, true);
+    m_shuff.add("hang volts", &CLIMB_INFO.RETAIN_VOLTS, true);
+
+    m_shuff.add("stow pos", &STOW_INFO.TARG_POS, true);
+    m_shuff.add("stow volts", &STOW_INFO.MOVE_VOLTS, true);
+    m_shuff.add("retain volts", &STOW_INFO.RETAIN_VOLTS, true);
+
+    m_shuff.add("extend pos", &EXTENDED_INFO.TARG_POS, true);
+    m_shuff.add("extend volts", &EXTENDED_INFO.MOVE_VOLTS, true);
+    m_shuff.add("keep extended volts", &EXTENDED_INFO.RETAIN_VOLTS, true);
 }
--- src/main/include/Climb/Climb.h
+++ src/main/include/Climb/Climb.h
@@ -22,14 +22,16 @@ class Climb : public Mechanism{
         };
 
         enum State{
-            MANUAL,
             MOVING,
             AT_TARGET,
+            WINDING
         };
 
     Climb();
     void CorePeriodic() override;
+    void CoreTeleopPeriodic() override;
     void CoreShuffleboardPeriodic() override;
+    void ManualPeriodic(double voltage);
     void PullUp();
     void Stow ();
     void Extend();
@@ -42,17 +44,29 @@ class Climb : public Mechanism{
         bool AtTarget(double target);
         TalonFX m_master {Ids::MASTER_CLIMB_MOTOR};//, m_slave {Ids::SLAVE_CLIMB_MOTOR};
         frc::DutyCycleEncoder m_absEncoder{Ids::CLIMB_ABS_ENCODER};
-        //electronic solenoid brake
 
         double m_pos;
         Target m_targ = STOWED;
         State m_state = AT_TARGET;
 
+        struct StateInfo{
+            double TARG_POS;
+            double MOVE_VOLTS;
+            double RETAIN_VOLTS;
+        };
+
         //CONSTANTS:
         double  MAX_VOLTS = 0.0, 
                 MIN_POS= 0.0, 
                 MAX_POS= 0.0, 
-                POS_TOLERANCE= 0.0, 
-                CLIMB_VOLTS= 0.0, 
-                HOLD_VOLTS= 0.0;
+                POS_TOLERANCE= 0.0;
+        StateInfo CLIMB_INFO =  {MIN_POS, 
+                            0.0, //climb volts
+                            0.0}; // hang volts (might be zero or stowed retain volts if using static hooks)
+        StateInfo STOW_INFO =    {MIN_POS,
+                            0.0, // stow/ravel volts 
+                            0.0}; // retain volts
+        StateInfo EXTENDED_INFO = {MAX_POS,
+                             0.0, // extend/unravel volts 
+                             0.0}; // prob 0
 };
