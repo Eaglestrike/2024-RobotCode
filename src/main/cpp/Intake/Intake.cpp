2da1dbabdb36f50aa00e1701efd7eb2ad0921cf7
linc-mvla <100027020@mvla.net>
2024-02-14 13:10:03 -0800

    remove made it out comment

--- src/main/cpp/Intake/Intake.cpp
+++ src/main/cpp/Intake/Intake.cpp
@@ -161,7 +161,7 @@ void Intake::SetState(ActionState newAction){
     if (newAction == AMP_INTAKE && (m_wrist.GetState() == Wrist::COAST || InIntake()) && m_channel.GetState() != Channel::OUT) return;
     if (newAction == PASSTHROUGH && InChannel()) return;
 
-    std::cout << "made it out " << std::endl;
+    // std::cout << "made it out " << std::endl;
 
     if (newAction == AMP_INTAKE && (InChannel() || m_channel.GetState() == Channel::OUT)) newAction = PASS_TO_AMP;
     if (m_actionState == AMP_INTAKE) m_timer = -1;
