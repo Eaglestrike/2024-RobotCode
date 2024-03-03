51d31a4dd5222353d337cc326a281a722f779e9a
Caleb L <degogolin@gmail.com>
2024-03-03 12:32:55 -0800

    speaker move

--- src/main/include/Constants/ShooterConstants.h
+++ src/main/include/Constants/ShooterConstants.h
@@ -149,7 +149,7 @@ namespace ShooterConstants{
     //Field Data
 
     //Speaker center positions (x, y)
-    const vec::Vector2D RED_SPEAKER = {16.54-0.15, 5.58};
+    const vec::Vector2D RED_SPEAKER = {16.54-0.30, 5.58};
     const vec::Vector2D BLUE_SPEAKER = {0.0, 5.58};
 
     //Dimensions of the shootable area
