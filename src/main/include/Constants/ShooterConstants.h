a59242ee9bd97dd51f542b02f2f7c6c57d749bcc
Caleb L <degogolin@gmail.com>
2024-04-02 14:51:27 -0700

    guess shoot while move

--- src/main/include/Constants/ShooterConstants.h
+++ src/main/include/Constants/ShooterConstants.h
@@ -202,7 +202,7 @@ namespace ShooterConstants{
     const vec::Vector2D ABSOLUTE_MISS = {10000000.0, 10000000.0}; //Forward kinematic miss
 
     //Constants for time calculation (t = kD * d + cT)
-    const double kD = 0.089123;
+    const double kD = 0.049123;
     const double cT = 0.177579;
     const double prepareT = 0.1;
 
