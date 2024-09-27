5529822fc3accdc77aecc72436182201db489b65
driverstation <me@sunayna.com>
2024-09-27 15:17:30 -0700

    changed id's

--- src/main/include/Constants/IntakeConstants.h
+++ src/main/include/Constants/IntakeConstants.h
@@ -1,10 +1,10 @@
 #pragma once
 
 namespace IntakeConstants { 
-  const int WRIST_MOTOR = 4;
+  const int WRIST_MOTOR = 22;
   const int ROLLER_MOTOR = 36;
   const int ROLLER_MOTOR_BACK = 14;
-  const int CHANNEL_MOTOR = 5;
+  const int CHANNEL_MOTOR = 2;
   const int KICKER_MOTOR = 2; 
   // const int WRIST_ENCODER_CAN_ID = 50; 
   const int BEAM_BREAK1_ID = 0;
